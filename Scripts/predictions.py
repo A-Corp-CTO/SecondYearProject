1816aac4542c7a7a1225e6f32ecbacd5eb0f31cc
Sneha Shrestha <snehashrestha@Snehas-MacBook-Air.local>
2023-04-17 10:15:19 +0200

    Baseline Predictions

---
+++ Scripts/predictions.py
@@ -0,0 +1,53 @@
+from rnn import read_data, data2feats
+import pickle
+import torch
+import sys
+import rnn
+def load_model():
+    with open('../Models/baseline_model.sav', 'rb') as f:
+        pipeline = pickle.load(f)
+        return pipeline
+
+model = load_model()
+
+def run_eval(feats_batches, labels_batches):
+    model.eval()
+    match = 0
+    total = 0
+    for sents, labels in zip(feats_batches, labels_batches):
+        output_scores = model.forward(sents)
+        predicted_tags  = torch.argmax(output_scores, 2)
+        for goldSent, predSent in zip(labels, predicted_tags):
+            for goldLabel, predLabel in zip(goldSent, predSent):
+                if goldLabel.item() != 0:
+                    total += 1
+                    if goldLabel.item() == predLabel.item():
+                        match+= 1
+    return(match/total)
+
+for devPath in sys.argv[1:]:
+    BATCH_SIZE=1
+    dev_data=read_data(devPath)
+    dev_feats, dev_labels = data2feats(dev_data, rnn.token_to_id, rnn.tag_to_id)
+    num_batches2 = int(len(dev_feats)/BATCH_SIZE)
+    max_len2=max([len(x[0]) for x in dev_data])
+
+    dev_feats_batches = dev_feats[:BATCH_SIZE*num_batches2].view(num_batches2, BATCH_SIZE, rnn.max_len)
+    dev_labels_batches = dev_labels[:BATCH_SIZE*num_batches2].view(num_batches2, BATCH_SIZE, rnn.max_len)
+    pred_tags = run_eval(dev_feats_batches, dev_labels_batches)
+
+save = False
+if save = True:
+    with open("literature_dev_predictions.txt",'w') as outfile:
+        for batchIdx in range(0, num_batches2):
+            input = dev_feats_batches[batchIdx]
+            output_scores = model.forward(input)
+            output_scores = output_scores.view(BATCH_SIZE * rnn.max_len, -1)
+            predicted_tags  = torch.argmax(output_scores, 1)
+            predicted_tags = predicted_tags.view(BATCH_SIZE, rnn.max_len,-1)
+            
+            for word, pred in zip(dev_data[batchIdx][0],predicted_tags.view(BATCH_SIZE * rnn.max_len, -1)):
+                if word != 0:
+                    outfile.write(word+"\t"+rnn.id_to_tag[pred]+"\n")
+
+            outfile.write("\n")
